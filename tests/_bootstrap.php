10c693294498093379bdf8678c3f0e9d98ebad2c
Andrii Vasyliev <sol@hiqdev.com>
2016-01-30 20:09:28 +0000

    fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -12,6 +12,3 @@
 error_reporting(E_ALL & ~E_NOTICE);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-Yii::setAlias('@hiqdev/yii2/merchant', dirname(__DIR__));
-Yii::setAlias('@hiqdev/yii2/merchant/tests', __DIR__);
