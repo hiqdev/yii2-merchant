bca73936be82a088dcef5c6e73263b3689533e46
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-02-03 15:19:04 +0200

    pay/deposit - added the message for a case when no payments methods available

--- src/views/pay/deposit.php
+++ src/views/pay/deposit.php
@@ -1,6 +1,7 @@
 <?php
 
 use hiqdev\yii2\merchant\widgets\PayButton;
+use yii\helpers\Html;
 
 $this->title                   = Yii::t('merchant', 'Select payment method');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('merchant', 'Recharge account'), 'url' => ['deposit']];
@@ -10,9 +11,13 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="row">
     <div class="col-md-4">
-        <?php foreach ($requests as $request): ?>
-            <?= PayButton::widget(compact('request')) ?>
-            <br/>
-        <?php endforeach ?>
+        <?php if (empty($requests)) {
+            echo Html::tag('div', Yii::t('merchant', 'There are no payments methods available'), ['class' => 'alert alert-danger']);
+        } else {
+            foreach ($requests as $request) {
+                echo PayButton::widget(compact('request'));
+                echo "<br/>";
+            }
+        } ?>
     </div>
 </div>
