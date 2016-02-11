99618c74329fff55812e1c2cc93b072377400858
Andrey Klochok <andreyklochok@gmail.com>
2016-02-11 14:04:19 +0200

    Minor fix. Add Box view

--- src/views/pay/deposit-form.php
+++ src/views/pay/deposit-form.php
@@ -3,19 +3,21 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-$this->title                   = Yii::t('merchant', 'Recharge account');
+$this->title = Yii::t('merchant', 'Recharge account');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-
 <div class="row">
-    <div class="col-md-4">
-        <?php $form = ActiveForm::begin() ?>
-            <?php foreach ($model->attributes() as $attr): ?>
-                <?= $form->field($model, $attr) ?>
-            <?php endforeach ?>
-            <?= Html::submitButton(Yii::t('merchant', 'Proceed'), ['class' => 'btn btn-primary']) ?>
-        <?php $form->end() ?>
+    <div class="col-md-offset-3 col-md-6">
+        <div class="box">
+            <div class="box-body">
+                <?php $form = ActiveForm::begin() ?>
+                <?php foreach ($model->attributes() as $attr): ?>
+                    <?= $form->field($model, $attr) ?>
+                <?php endforeach ?>
+                <?= Html::submitButton(Yii::t('merchant', 'Proceed'), ['class' => 'btn btn-primary']) ?>
+                <?php $form->end() ?>
+            </div>
+        </div>
     </div>
-
 </div>
--- src/views/pay/deposit.php
+++ src/views/pay/deposit.php
@@ -3,21 +3,26 @@
 use hiqdev\yii2\merchant\widgets\PayButton;
 use yii\helpers\Html;
 
-$this->title                   = Yii::t('merchant', 'Select payment method');
+$this->title = Yii::t('merchant', 'Select payment method');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('merchant', 'Recharge account'), 'url' => ['deposit']];
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-
 <div class="row">
-    <div class="col-md-4">
-        <?php if (empty($requests)) : ?>
-            <?= Html::tag('div', Yii::t('merchant', 'There are no payments methods available'), ['class' => 'alert alert-danger']) ?>
-        <?php else : ?>
-            <?php foreach ($requests as $request) : ?>
-                <?= PayButton::widget(compact('request')) ?>
-                <br/>
-            <?php endforeach ?>
-        <?php endif ?>
+    <div class="col-md-offset-3 col-md-6">
+        <div class="box <?= empty($requests) ? 'box-danger' : '' ?>">
+            <div class="box-body">
+
+                <?php if (empty($requests)) : ?>
+                    <?= Html::tag('div', Yii::t('merchant', 'There are no payments methods available'), ['class' => 'alert alert-danger']) ?>
+                <?php else : ?>
+                    <?php foreach ($requests as $request) : ?>
+                        <?= PayButton::widget(compact('request')) ?>
+                        <br/>
+                    <?php endforeach ?>
+                <?php endif ?>
+
+            </div>
+        </div>
     </div>
 </div>
