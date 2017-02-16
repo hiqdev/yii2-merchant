820df723af69ed939a99ebfbf157552f19e7cdfb
tofid <andreyklochok@gmail.com>
2017-02-16 17:31:53 +0200

    Added class clearly

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -3,6 +3,7 @@
 return [
     'components' => [
         'themeManager' => [
+            'class' => \hiqdev\thememanager\ThemeManager::class,
             'pathMap' => [
                 '@hiqdev/yii2/merchant/views' => '$themedViewPaths',
             ],
