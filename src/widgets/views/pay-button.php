250ba1ed36594679c8778370b3cef0ae7d5a6b2a
tofid <andreyklochok@gmail.com>
2017-03-23 12:00:32 +0200

    Removed commented code

--- src/widgets/views/pay-button.php
+++ src/widgets/views/pay-button.php
@@ -17,25 +17,6 @@ use yii\widgets\ActiveForm;
 <?= Html::hiddenInput('type', $request->getType()) ?>
 <?= Html::hiddenInput('data', Json::encode($request->data)) ?>
 
-<!--button class="btn btn-default btn-block" type="submit" style="text-align:left; white-space: normal;">
-    <i class="pi pi-sm pi-<?= $request->merchant->getSimpleName() ?>" style="float:right"></i>
-    <br/>
-    <?= Yii::t('merchant', 'Pay {amount} with {merchantLabel}', [
-    'amount' => Html::tag('b', $widget->formatMoney($request->getAmount())),
-    'merchantLabel' => Html::tag('b', $request->merchant->getLabel()),
-]); ?>
-    <br/>
-
-    <?php if ($request->getTotalFee() > 0) : ?>
-        (<?= Yii::t('merchant', 'including commission {commission}', [
-    'commission' => Html::tag('b', $widget->formatMoney($request->getTotalFee())),
-]) ?>)
-    <?php endif ?>
-    <br/>
-    <?php $widget->renderButtonComment() ?>
-    <br/>
-</button-->
-
 <button type="submit" class="btn-block">
     <div class="product-img">
         <i class="pi pi-sm pi-<?= $request->merchant->getSimpleName() ?>" style="float:right"></i>
