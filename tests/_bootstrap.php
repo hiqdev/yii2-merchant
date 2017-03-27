9c8aaead103e79064694503519df94323f56302d
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-03-27 16:53:47 +0300

    Added PHPUnit 6 compatibility

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,15 +1,29 @@
 <?php
-
-/*
- * Yii2 extension for payment processing with Omnipay, Payum and more later
+/**
+ * PHPUnit plugin for HiDev.
  *
- * @link      https://github.com/hiqdev/yii2-merchant
- * @package   yii2-merchant
+ * @link      https://github.com/hiqdev/hidev-phpunit
+ * @package   hidev-phpunit
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 error_reporting(E_ALL & ~E_NOTICE);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+/*
+ * Ensures compatibility with PHPUnit 6.x
+ */
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
